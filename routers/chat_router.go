925e708edfba9b0dee21cd13bf5791f2d95fd84e
lijiang <zhourong976@gmail.com>
2024-11-24 16:38:12 +0800

    1.群聊的内容新增

--- routers/chat_router.go
+++ routers/chat_router.go
@@ -8,4 +8,6 @@ func (router RouterGroup) ChatRouter() {
 	app := api.ApiGroupApp.ChatApi
 	router.GET("chat_groups", app.ChatGroupView)
 	router.GET("chat_groups_records", app.ChatListView)
+	router.GET("chat_groups_remove", app.ChatRemoveView)
+
 }
